7a2a3a7936131521f8a602557463fd6ac9b55d05
Eyrma <erotstein@outlook,com>
2024-02-21 21:04:24 -0800

    updating login and signup function in App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React { useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Food from './Components/Food';
 import NewFood from './Components/NewFood';
 import Home from './Components/Home';
 import NewReview from './Components/NewReview';
 import Grocery from './Components/Grocery';
-//import SignIn from './Components/SignIn';//
+import Login from './Components/Login';
+import SignUp from './Components/SignUp.js';
 import Cart from './Components/Cart.js';
+
 
 //Home, New, FoodPage
 
 function App() {
+  var [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('loggedin'));
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  }
+
+  const login = () => {
+    setIsLoggedIn(true);
+  }
+
   return (
     <div>
          <Router>
-          <Navbar />
+         <Navbar isLoggedIn={isLoggedIn} logout={logout} key={isLoggedIn}/>
           <Routes>
             <Route path ='/' element={<Home />}/>
             <Route path ='/food' element={<Food />}/>
             <Route path ='/food/new' element={<NewFood/>}/>
             <Route path ='/food/review' element={<NewReview/>} />
             <Route path = '/Grocery' element={<Grocery/>}/>
-
-            {/*<Route path = '/SignIn' element={<SignIn/>}/>*/}
+            <Route path = '/login' element={isLoggedIn ? <Navigate to="/"/> : <Login login={login}/>}/>
+            <Route path = '/signup' element={isLoggedIn ? <Navigate to="/"/> : <SignUp/>}/>
             <Route path ='/Cart'  element={<Cart/>}/>
-
-
-            <Route path = '/Login' element={<Login/>}/>
             <Route path = '/food/random' element={<RandomFood/>}/>
-          </Routes>
+        </Routes>
+        <Footer/>
          </Router>
     </div>
   );
