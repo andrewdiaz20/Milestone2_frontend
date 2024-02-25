95a09c56571ef4ce30c59c4130e3800e829de22b
Daniel Flores <DannniF>
2024-02-25 09:07:39 -0800

    banner fix showing up with image

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,8 +1,34 @@
 import React from 'react'
-import Slider from 'react-slider'
-import {Link }from 'react-dom'
+import Slider from 'react-slick'
+import Link from 'react-router-dom'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import '../CSS/Banner.css'
+import image10001 from '../image/10001.webp';
+import image10002 from '../image/10002.webp';
 
+function CustomNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
 
 function Banner() {
 
@@ -10,14 +36,14 @@ function Banner() {
         {
     id:1, 
     title:"fresh bread",
-    image:'../image/10001.webp',
+    image:image10001,
     discription:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
 },{
     id:2,
-    image: "../image/10002.jpg",
-    title:'cheases',
+    image: image10002,
+    title:'cheese',
     discription:'our cheeses are made with the finest ingredients.',
-    button:'Shop Now!'
+    // button:'Shop Now!'
 }
 
 ]
@@ -27,22 +53,26 @@ let settings = {
     infinite: true,
     speed:500,
     slidesToShow:1,
-    slidesToScroll:1
+    slidesToScroll:1,
+    nextArrow: <CustomNextArrow/>,
+    prevArrow: <SamplePrevArrow/>
+    
+    
 };
 
 
   return (
-    <div className='bannerslider'>
-       <Slider className='bannerslider' {...settings}>
+    <div >
+       <Slider  {...settings}>
 {
     data.map(item=>{
         return(
-            <div className='imagecont' key={item.id}>
+            <div className='imagecont bannerslider' key={item.id}>
                 <img src={item.image} alt='noimg'/>
                    <div className='content'>
                    <h3>{item.title}</h3>
                    <span>{item.discription}</span>
-                   <button>shope more</button>
+                   <button>shop more</button>
                    </div>
             </div>
         )
